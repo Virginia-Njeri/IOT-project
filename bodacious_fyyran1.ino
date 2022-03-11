cd9b58f213c1420b25ac6f98409d94ebb23b5375
virginia-njeri <99467877+virginia-njeri@users.noreply.github.com>
2022-03-11 08:49:55 +0300

    Add files via upload

---
+++ bodacious_fyyran1.ino
@@ -0,0 +1,15 @@
+// C++ code
+int red=1;
+//
+void setup()
+{
+  pinMode(1, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(red, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(red, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
