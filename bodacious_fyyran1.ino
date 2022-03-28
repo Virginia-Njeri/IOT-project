c322773e092483af8d435a5ef9b4aa8644f19cf0
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-03-28 22:22:32 +0300

    changes

--- bodacious_fyyran1.ino
+++ bodacious_fyyran1.ino
@@ -9,7 +9,7 @@ void setup()
 void loop()
 {
   digitalWrite(red, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); // Wait for 5000 millisecond(s)
   digitalWrite(red, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); // Wait for 5000 millisecond(s)
 }
